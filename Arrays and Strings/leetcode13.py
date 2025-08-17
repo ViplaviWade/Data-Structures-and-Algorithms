fd036e1be7af6a04a42d1a13df8139a7e0ce1a6e
ViplaviWade <viplaviwade21@gmail.com>
2025-08-17 21:51:27 +0100

    Leetcode 13

---
+++ Arrays and Strings/leetcode13.py
@@ -0,0 +1,14 @@
+# Roman to Integer
+
+def romanToInt(s):
+    roman = { 'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000 }
+    num = 0
+    for i in range(len(s)):
+        if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+            num -= roman[s[i]]
+        else:
+            num += roman[s[i]]
+    return num
+
+num = romanToInt(s='IV')
+print(num)
